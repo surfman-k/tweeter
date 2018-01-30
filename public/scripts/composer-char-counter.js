0f173daf732c41783f9cfbe909978c7321dc1b1a
Konrad <konrad.kipczak@gmail.com>
2018-01-30 01:09:53 +0000

    the counter now changes to red if it is below 0 characters and back to black if 0 or more

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,11 @@ $( document ).ready(function() {
 	$( "#new-tweet" ).keyup(function() {
 	  let counterVal = $(this).parent().find('.counter');
 	  counterVal.text(140 - ($(this).val().length));
+	  if (counterVal.text() < 0){
+	  	counterVal.css({color: "red"});
+	  } else {
+	  	counterVal.css({color: "black"});
+	  }
 	});
 
 });
